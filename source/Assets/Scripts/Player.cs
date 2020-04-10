6649cad810c53cb55ad4503bcea5dd6030187dc5
mwfj <wufangm@clemson.edu>
2020-04-09 20:27:43 -0400

    usupport the angle up and mouse wheel scroll

--- source/Assets/Scripts/Player.cs
+++ source/Assets/Scripts/Player.cs
@@ -13,9 +13,17 @@ public class Player : MonoBehaviour
     private Vector3 cameraVelocity = Vector3.zero;
     private float rotateSpeed = 400;
     private float cameraRoate = 0;
-    private float cameraSpeed = 0;
+    // private float cameraSpeed = 0;
     private Vector3 velocity = Vector3.zero;
 
+    // The check whether in the look up mode
+    private bool isLookUp;
+
+    //Limit the range of field of view for Player Camera
+    public float minFov = 30f;
+    public float maxFov = 80f;
+    //The distance range that the camera moves after a mouse wheel
+    public float sensitivity = 10f;
     void Start()
     {
         Spawn();
@@ -64,18 +72,46 @@ public class Player : MonoBehaviour
 
 
     }
+
+     // After scrolling the mouse wheel, it field of view for player camer will be changed
+    private void ScrollWheel(){
+        float fov = playerCam.fieldOfView;
+        fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        playerCam.fieldOfView = fov;
+    }
     private void UpdateCamera()
     {
         // use smooth damp to move camera to target position smoothly
         //float k = Mathf.Max((this.transform.position - offset).magnitude,0);
        
 
-
+        //Rotate by Y axis
         if (Input.GetKey(KeyCode.Mouse1))
         {
             float mouseX = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
             playerCam.transform.RotateAround(this.transform.position, Vector3.up, mouseX);
         }
+
+        //When the user clicks the left mouse button, the camera will go from bottom to top to view the character
+        //When the user clicks it again, the relative position of camera back to the default angle. 
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            // float mouseY = Input.GetAxis("Mouse Y") * rotateSpeed* 4 * Time.deltaTime;
+            // playerCam.transform.RotateAround(this.transform.position, Vector3.right, mouseY);
+            if(isLookUp){
+                isLookUp=false;
+            }else{
+                isLookUp=true;
+            }
+        }
+        if(isLookUp){
+            playerCam.transform.position += -playerCam.transform.up *2.5f*Time.deltaTime;
+        }
+        // if(Input.GetAxis("Mouse ScrollWheel")!=0){
+        //     playerCam.fieldOfView = playerCam.fieldOfView-Input.GetAxis("Mouse ScrollWheel")*20;
+        // }
+        ScrollWheel();
+        
         cameraRoate = playerCam.transform.eulerAngles.y / 180 * Mathf.PI;
         float sr = Mathf.Sin(cameraRoate);
         float cr = Mathf.Cos(cameraRoate);
